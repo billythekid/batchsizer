0da173434eef1b6d31e303c5e2efe0a3521dfd3f
billythekid <billy@the-kid.org>
2016-01-21 13:38:38 +0000

    logic bug

--- resources/views/_includes/nav.blade.php
+++ resources/views/_includes/nav.blade.php
@@ -1,50 +1,51 @@
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
 
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name') }}
-                </a>
-            </div>
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name') }}
+            </a>
+        </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                {{--
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            {{--
 
-                 <ul class="nav navbar-nav">
-                     <li><a href="{{ url('/home') }}">Home</a></li>
-                 </ul>
-                --}}
+             <ul class="nav navbar-nav">
+                 <li><a href="{{ url('/home') }}">Home</a></li>
+             </ul>
+            --}}
 
-                <div class="nav navbar-nav navbar-right">
-                    @if (Auth::guest() && App::environment('local'))
-
+            <div class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    @if (app()->environment('local'))
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
                     @endif
-                    <li>
-                        <a href="#" onclick="showFeedbackForm()">Feedback</a>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
                     </li>
-                </div>
+                @endif
+                <li>
+                    <a href="#" onclick="showFeedbackForm()">Feedback</a>
+                </li>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
