d5c7619a2a5cd122c140fb3e2660da391e165f87
billythekid <billy@the-kid.org>
2016-01-31 20:26:29 +0000

    unhid login link, added plans to login page, activated project level sign-up

--- resources/views/_includes/nav.blade.php
+++ resources/views/_includes/nav.blade.php
@@ -24,26 +24,25 @@
             --}}
 
             <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false"><i class="fa fa-btn fa-user"></i>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                    @if (Auth::guest())
-                        @if (app()->environment('local'))
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                        @endif
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false"><i class="fa fa-btn fa-user"></i>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                    <li>
-                        <button class="btn btn-info navbar-btn" onclick="showFeedbackForm()"><i class="fa fa-btn fa-comment"></i> Feedback</button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
+                        </ul>
                     </li>
+                @endif
+                <li>
+                    <button class="btn btn-info navbar-btn" onclick="showFeedbackForm()"><i
+                                class="fa fa-btn fa-comment"></i> Feedback
+                    </button>
+                </li>
 
             </ul>
         </div>
