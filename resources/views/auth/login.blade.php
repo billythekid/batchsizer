96b89e9c9fab5354adae0ff2825f5e31bad70efa
billythekid <billy@the-kid.org>
2016-02-18 09:40:37 +0000

    link to examples on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,9 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">Not yet registered?</div>
                     <div class="panel-body">
-                        <p>Our accounts give a number of additional benefits and functionality.</p>
+                        <p>Our accounts give a number of additional benefits and functionality. To see some examples of
+                            the additional things you can do to your images with an account, check out our
+                            <a href="{{ route('examples') }}">examples page <i class="fa fa-link"></i></a></p>
                         @include('_includes.plans')</div>
                 </div>
 
